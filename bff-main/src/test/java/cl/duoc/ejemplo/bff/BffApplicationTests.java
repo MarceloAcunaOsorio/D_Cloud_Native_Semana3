1b256df862fc7a7727e4410be9a98ee1d6a3d069
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-01-19 19:48:13 -0300

    se conecta a base de datos

--- bff-main/src/test/java/cl/duoc/ejemplo/bff/BffApplicationTests.java
+++ bff-main/src/test/java/cl/duoc/ejemplo/bff/BffApplicationTests.java
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BffApplicationTests {
+	
 
 	@Test
 	void contextLoads() {
+		  // Método vacío porque aún no se ha implementado
+    // Este método se dejará vacío por ahora ya que es parte de una funcionalidad pendiente de implementación.
 	}
 
 }
